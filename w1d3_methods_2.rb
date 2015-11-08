e52da76d78de5bbe64ac1631c62c43e7645c7e1c
Michel Frechette <mifrec@hotmail.com>
2015-11-08 15:36:17 -0500

    add conditional if statment, return keyword produces value

--- w1d3_methods_2.rb
+++ w1d3_methods_2.rb
@@ -7,3 +7,11 @@ def add10(first, second = 10)
   "#{first + second} is the sum of the addition."
   return
 end
+
+def sum(first, second)
+  if first > second
+    return first + second
+  else
+    return first - second
+  end
+end
